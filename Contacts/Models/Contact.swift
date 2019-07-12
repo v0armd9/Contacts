811e62fa0111b026013b74e620e5e5c41ffa862e
Darin Armstrong <darin.marmstrong@gmail.com>
2019-07-12 11:01:43 -0600

    Created model object for contact

---
+++ Contacts/Models/Contact.swift
@@ -0,0 +1,55 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Darin Marcus Armstrong on 7/12/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+//ContactConstants
+struct ContactConstants {
+    static let recordType = "Contact"
+    fileprivate static let nameKey = "Name"
+    fileprivate static let phoneNumberKey = "PhoneNumber"
+    fileprivate static let emailKey = "Email"
+    fileprivate static let recordIDKey = "RecordID"
+}
+
+class Contact {
+    
+    //MARK: - Class Properties
+    var name: String
+    var phoneNumber: String
+    var email: String
+    var recordID: CKRecord.ID
+    
+    //Designated Initializer
+    init(name: String, phoneNumber: String, email: String, recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
+        
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.recordID = recordID
+    }
+    
+    convenience init?(record: CKRecord) {
+        guard let name = record[ContactConstants.nameKey] as? String,
+        let phoneNumber = record[ContactConstants.phoneNumberKey] as? String,
+        let email = record[ContactConstants.emailKey] as? String
+            else {return nil}
+        
+        self.init(name: name, phoneNumber: phoneNumber, email: email, recordID: record.recordID)
+    }
+}
+
+extension CKRecord {
+    convenience init(contact: Contact) {
+        self.init(recordType: ContactConstants.recordType, recordID: contact.recordID)
+        self.setValue(contact.name, forKey: ContactConstants.nameKey)
+        self.setValue(contact.phoneNumber, forKey: ContactConstants.phoneNumberKey)
+        self.setValue(contact.email, forKey: ContactConstants.emailKey)
+    }
+}
